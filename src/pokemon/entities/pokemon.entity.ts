6c987f7a2e11037bbcc8b4569b78cd290c01fb76
Carles Labrana <carles.labrana@idealiveconsulting.com>
2025-01-23 11:35:22 +0100

    DOCKER - MONGODB - STARTUP

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,23 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+    //id: string;   //Mongo da el ID unico
+    @Prop({
+        unique: true,
+        index: true
+    })
+    no: number;     //Numero del pokemon
+
+    @Prop({
+        unique: true,
+        index: true
+    })
+    name: string;
+    
+    url: string;
+}
+
+//Eportamos esquema de definiciones
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
